77eeda720d397d1b77e5c940dc3d37c28e8a0bd9
hilow <252114997@qq.com>
2014-10-26 21:13:54 +0800

    1.增加使用BFS广度优先遍历计算树高的方法

--- leetcode/maximum-depth-of-binary-tree.cpp
+++ leetcode/maximum-depth-of-binary-tree.cpp
@@ -1,10 +1,3 @@
-/* ******************************************************************
-
-This source code is provided as part of article "using STL vectors to create Binary Search Trees (BST)" written
-by Deepak Kumar Gupta. This file inherits the licence of CP Licence
-
-****************************************************************************/
-
 /*
 https://oj.leetcode.com/problems/maximum-depth-of-binary-tree/
 Given a binary tree, find its maximum depth.
@@ -15,12 +8,25 @@ The maximum depth is the number of nodes along the longest path from the root no
 源码中的二叉树引用了http://www.codeproject.com/Articles/602805/Creating-a-binary-search-tree-B 
 可用最简单的递归法解决；
 或者广度优先遍历（BFS)
-或者深度优先遍历（DFS)
-reference:http://blog.csdn.net/ithomer/article/details/8799795
+但我觉得更简单的办法是在树节点中添加size，表示子节点个数。
+查找时一直向size大的节点走。如此效率最高速度最快。
+
+reference:
+http://blog.csdn.net/ithomer/article/details/8799795
+http://zh.wikipedia.org/wiki/%E6%B7%B1%E5%BA%A6%E4%BC%98%E5%85%88%E6%90%9C%E7%B4%A2
 */
+
+/* ******************************************************************
+
+This source code is provided as part of article "using STL vectors to create Binary Search Trees (BST)" written
+by Deepak Kumar Gupta. This file inherits the licence of CP Licence
+
+****************************************************************************/
+
 
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 struct bst
@@ -111,13 +117,46 @@ void PostTrav(vector <struct bst> &v1, unsigned int Idx)
 	cout<<v1[Idx].data<<endl;
 }
 
-unsigned int MaxDepth(vector <struct bst> &v1, unsigned int Idx)
+// 使用Queue 实现 BFS 广度优先遍历
+unsigned int MaxDepthBfs(vector <struct bst> &nodes, unsigned int Idx) 
+{
+	unsigned int depth = 0;
+	unsigned int child_num = 0;
+	queue<unsigned int> unvisited;
+
+	if (-1 == Idx) {
+		return 0;
+	}
+
+	unvisited.push(Idx);
+	child_num = unvisited.size();
+	while (!unvisited.empty()) {
+		unsigned int cur = unvisited.front();
+		unvisited.pop();
+		if (-1 != nodes[cur].leftIdx) {
+			unvisited.push(nodes[cur].leftIdx);
+		}
+		if (-1 != nodes[cur].rightIdx) {
+			unvisited.push(nodes[cur].rightIdx);
+		}
+
+		child_num--;
+		if (0 == child_num) { // child_num==0 时，表明已经完成树中某一层高度的所有节点的遍历,也就是又加深了一层
+			depth++;
+			child_num = unvisited.size();
+		}
+	}
+	return depth;
+}
+
+// 使用递归实现 DFS 深度优先遍历
+unsigned int MaxDepth(vector <struct bst> &nodes, unsigned int Idx)
 {
 	if (-1 == Idx) {
 		return 0;
 	}
-	unsigned int left = MaxDepth(v1, v1[Idx].leftIdx);
-	unsigned int right = MaxDepth(v1, v1[Idx].rightIdx);
+	unsigned int left = MaxDepth(nodes, nodes[Idx].leftIdx);
+	unsigned int right = MaxDepth(nodes, nodes[Idx].rightIdx);
 	return left > right ? (left+1) : (right+1);
 }
 
@@ -138,7 +177,10 @@ int main()
 	PreTrav(v1,0);
 	cout<<"PostTrav:"<<endl;
 	PostTrav(v1,0);
+
 	cout<<"MaxDepth:"<<MaxDepth(v1, 0)<<endl;
+	cout<<"MaxDepthBFS:"<<MaxDepthBfs(v1, 0)<<endl;
+
 	return 0;
 }
 
