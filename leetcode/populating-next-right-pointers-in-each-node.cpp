797f3ff3594e86337b1c603c032e1db71b4b7b37
hilow <252114997@qq.com>
2014-11-30 15:10:18 +0800

    1.add comments

--- leetcode/populating-next-right-pointers-in-each-node.cpp
+++ leetcode/populating-next-right-pointers-in-each-node.cpp
@@ -1,51 +1,55 @@
 /**
- * @brief 
+ * @brief https://oj.leetcode.com/problems/populating-next-right-pointers-in-each-node/
  * 
- * Given a binary tree 
-    struct TreeLinkNode {
-      TreeLinkNode *left;
-      TreeLinkNode *right;
-      TreeLinkNode *next;
-    }
-
-
-
-Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
-
-Initially, all next pointers are set to NULL.
-
-Note: 
-•You may only use constant extra space.
-•You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
-
-
-For example,
- Given the following perfect binary tree,
-
-         1
-       /  \
-      2    3
-     / \  / \
-    4  5  6  7
-
-
-
-After calling your function, the tree should look like:
-
-         1 -> NULL
-       /  \
-      2 -> 3 -> NULL
-     / \  / \
-    4->5->6->7 -> NULL
-
-
-解：
-*/
+ *  Given a binary tree 
+ *     struct TreeLinkNode {
+ *       TreeLinkNode *left;
+ *       TreeLinkNode *right;
+ *       TreeLinkNode *next;
+ *     }
+ * 
+ * 
+ * 
+ * Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+ * 
+ * Initially, all next pointers are set to NULL.
+ * 
+ * Note: 
+ * ?You may only use constant extra space.
+ * ?You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+ * 
+ * 
+ * For example,
+ *  Given the following perfect binary tree,
+ * 
+ *          1
+ *        /  \
+ *       2    3
+ *      / \  / \
+ *     4  5  6  7
+ * 
+ * 
+ * 
+ * After calling your function, the tree should look like:
+ * 
+ *          1 -> NULL
+ *        /  \
+ *       2 -> 3 -> NULL
+ *      / \  / \
+ *     4->5->6->7 -> NULL
+ * 
+ * 
+ * 解：
+ * 只要在遍历树节点时知道当前节点的depth即可将其链接到一起。
+ * 不过无论preorder、inorder、postorder，都是先访问left再访问right，但题目要求从left链接到next中，
+ * 所以比较方便的方法是先访问right，再访问left，这样每次只需要将当前节点的next指针指向上一次访问的节点即可。
+ * 另外，bfs或dfs遍历都可以实现，我用的是bfs
+ */
 
 #include <vector>
 #include <stdio.h>
-#include "include/bstree.hpp"
 
+#include "include/bstree.hpp"
 int main()
 {
     bstree tree;
